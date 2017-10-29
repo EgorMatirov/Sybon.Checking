6db1a64da403dd71600e74399482e6ed3fd85569
Mikhail Ivchenko <ematirov@gmail.com>
2017-10-29 21:49:49 +0400

    Add client generation

---
+++ Sybon.Checking.Client/generate.py
@@ -0,0 +1,6 @@
+import subprocess, time, signal, os, sys
+projectName = 'Sybon.Checking'
+p = subprocess.Popen(["dotnet", "run"], cwd = os.path.join(os.getcwd(), projectName))
+time.sleep(20)
+subprocess.call(["java", "-jar", ".\\" + projectName + ".Client\\swagger-codegen-cli-3.0.0-20170904.171256-3.jar", "generate", "-l", "csharp", "--additional-properties", "targetFramework=v5.0,netCoreProjectFile=true,packageName="+projectName+".Client", "-i", "http://localhost:8194/swagger/v1/swagger.json", "-o", projectName+".Client\\"+projectName+".Client"])
+p.send_signal(signal.CTRL_C_EVENT)
