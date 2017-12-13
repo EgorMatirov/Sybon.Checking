37f32ea0f85504d1ccb2ed4ee6b87cc18c6ad1cd
Mikhail Ivchenko <ematirov@gmail.com>
2017-12-13 15:44:34 +0400

    Remove dead code

--- Sybon.Checking/Services/SubmitResultService/SubmitResultService.cs
+++ Sybon.Checking/Services/SubmitResultService/SubmitResultService.cs
@@ -22,47 +22,5 @@ namespace Sybon.Checking.Services.SubmitResultService
             var dbEntry = await _repositoryUnitOfWork.GetRepository<ISubmitResultRepository>().FindAsync(id);
             return _mapper.Map<SubmitResult>(dbEntry);
         }
-//
-//        public override long Add(SubmitResult entry)
-//        {
-//            var dbEntry = MapperUoW.SubmitResultMapper.InverseMap(entry);
-//            AddProps(dbEntry);
-//            dbEntry = RepositoryUoW.SubmitResultRepository.Add(dbEntry);
-//            RepositoryUoW.Save();
-//            return dbEntry.Id;
-//        }
-//
-//        public override void Update(SubmitResult entry)
-//        {
-//            var dbEntry = MapperUoW.SubmitResultMapper.InverseMap(entry);
-//            AddProps(dbEntry);
-//            dbEntry = RepositoryUoW.SubmitResultRepository.Update(dbEntry);
-//            RepositoryUoW.Save();
-//        }
-//
-//        public override void Remove(long id)
-//        {
-//            RepositoryUoW.SubmitResultRepository.Remove(id);
-//        }
-//
-//        protected virtual void AddProps(Db.SubmitResult dbEntry)
-//        {
-//            var buildResultRepo = RepositoryUoW.GetRepo<Db.BuildResult>();
-//            var testGroupResultRepo = RepositoryUoW.GetRepo<Db.TestGroupResult>();
-//            var resourceUsageRepo = RepositoryUoW.GetRepo<Db.ResourceUsage>();
-//
-//            dbEntry.BuildResult = buildResultRepo.Add(dbEntry.BuildResult);
-//            foreach (var testGroupResult in dbEntry.TestResults)
-//            {
-//                foreach (var testResult in testGroupResult.TestResults.Where(x => x.ResourceUsage != null))
-//                {
-//                    testResult.ResourceUsage = resourceUsageRepo.Add(testResult.ResourceUsage);
-//                    RepositoryUoW.Save();
-//                }
-//                testGroupResult.SubmitResultId = dbEntry.Id;
-//            }
-//            dbEntry.TestResults = testGroupResultRepo.AddRange(dbEntry.TestResults);
-//            RepositoryUoW.Save();
-//        }
     }
 }
